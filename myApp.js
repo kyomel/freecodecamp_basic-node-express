3e60321b95b1f9b962ea8ff47b4db3780e2f3c66
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-04-09 15:49:50 +0700

    [kyomel] done part 1

--- myApp.js
+++ myApp.js
@@ -1,12 +1,42 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+require('dotenv').config();
+
+app.use(express.static('public'));
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip)
+    next();
+  })
+  
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.send(res.json({time: req.time}));
+})
 
 
-
-app.get('/', function(req, res) {
+app.get('/test', function(req, res) {
     res.send("Hello Express");
 })
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+})
+
+app.get('/json', function(req, res) {
+    res.json({"message": "Hello json"})
+})
+
+app.get('/json', function(req, res) {
+    let message = "Hello json";
+    if(process.env.MESSAGE_STYLE === 'uppercase') {
+        return res.json({'message': message.toUpperCase()});
+    }
+    return res.status(200).json({'message': message})
+  })
+  
 
 
 
