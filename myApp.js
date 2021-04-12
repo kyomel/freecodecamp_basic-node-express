b949a792b0274b0bfe97a245591c1f0ba17cf12e
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-04-12 16:41:54 +0700

    [kyomel] done section about express

--- myApp.js
+++ myApp.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
 require('dotenv').config();
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip)
@@ -35,7 +40,26 @@ app.get('/json', function(req, res) {
         return res.json({'message': message.toUpperCase()});
     }
     return res.status(200).json({'message': message})
-  })
+})
+
+app.get('/:word/echo', function(req,res) {
+    let { word } = req.params;
+    res.json({
+        echo: word
+    });
+})
+
+app.get('/name', function(req, res) {
+    let first = req.query.firs;
+    let last = req.query.last;
+    let objRes = {name:`${first} ${last}`};
+    res.send(objRes);
+})
+
+app.post('/name', function(req, res) {
+    let string  = req.body.first + " " + req.body.last;
+    res.json({ name: string});
+})
   
 
 
